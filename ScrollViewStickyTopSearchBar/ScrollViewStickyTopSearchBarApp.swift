35daf81c70850dc5ca85aaccd1eb9a44857b3432
Max <maxim.macari.dam@gmail.com>
2021-03-19 11:26:29 +0100

    Initial Commit

---
+++ ScrollViewStickyTopSearchBar/ScrollViewStickyTopSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewStickyTopSearchBarApp.swift
+//  ScrollViewStickyTopSearchBar
+//
+//  Created by Maxim Macari on 19/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewStickyTopSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
